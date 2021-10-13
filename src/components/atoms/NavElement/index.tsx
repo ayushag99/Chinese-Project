cf41a4f805cbe021a60a7371ef313ce039c2fb66
Ayush Agrawal <ayush.ashuvaish@gmail.com>
2021-10-13 07:19:03 +0530

    add: NavElement

---
+++ src/components/atoms/NavElement/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Importing styles
+import styles from "style.module.css";
+
+const NavElement: React.FC<{ onClickHandler?: () => {}, link?: string }> = (
+  props
+) => {
+  return (
+    <a
+      href={props.link}
+      className={styles.navElement}
+      onClick={props.onClickHandler}
+    >
+      {props.children}
+    </a>
+  );
+};
+export default NavElement;
